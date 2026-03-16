4232ad7ead01c8b7dbc3059f7841e2217540fbfe
Nikolaos Makridis <makridisnikolas@icloud.com>
2026-03-16 20:52:11 +0000

    dealt with first feature requirement: The Registration feature, you must be able to register new users, users with the same name can not exist (names must be unique). They must be saved in the right file.

--- atm-system/src/auth.c
+++ atm-system/src/auth.c
@@ -57,3 +57,63 @@ const char *getPassword(struct User u)
     fclose(fp);
     return "no user found";
 }
+
+void registerMenu(char a[50], char pass[50])
+{
+    struct termios oflags, nflags;
+    struct User u;
+    FILE *fp;
+    int maxId = -1;
+
+    system("clear");
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t Register:");
+    scanf("%s", a);
+
+    // check for duplicate username
+    if ((fp = fopen(USERS, "r")) != NULL)
+    {
+        while (fscanf(fp, "%d %s %s", &u.id, u.name, u.password) != EOF)
+        {
+            if (strcmp(u.name, a) == 0)
+            {
+                fclose(fp);
+                printf("\n\t\t\tUsername already exists. Please choose another.\n");
+                exit(1);
+            }
+            if (u.id > maxId)
+                maxId = u.id;
+        }
+        fclose(fp);
+    }
+
+    // hidden password input
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
+    {
+        perror("tcsetattr");
+        exit(1);
+    }
+    printf("\n\n\n\n\n\t\t\t\tEnter a password:");
+    scanf("%s", pass);
+
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    {
+        perror("tcsetattr");
+        exit(1);
+    }
+
+    // save new user
+    if ((fp = fopen(USERS, "a")) == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    fprintf(fp, "%d %s %s\n", maxId + 1, a, pass);
+    fclose(fp);
+
+    printf("\n\t\t\tAccount created successfully! Welcome, %s.\n", a);
+}
